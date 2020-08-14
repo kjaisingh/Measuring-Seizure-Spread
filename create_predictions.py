39b8f1cc3a91f1fd300851bccd7c536613de8e63
Karan Jaisingh <kj.jaisingh@gmail.com>
2020-08-14 15:21:19 +0800

    Restructured data

---
+++ create_predictions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 14 15:06:41 2020
+
+@author: jaisi8631
+"""
+
+"""
+PRIOR TO RUNNING THIS FILE:
+1. Download dataset 
+    
+    
+"""
