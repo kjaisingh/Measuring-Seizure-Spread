94797007bd7e12f13262b6d4127c5195c6b1cf3f
Karan Jaisingh <kj.jaisingh@gmail.com>
2020-08-14 15:51:15 +0800

    Imports of new datasets for testing

--- create_predictions.py
+++ create_predictions.py
@@ -6,14 +6,82 @@ Created on Fri Aug 14 15:06:41 2020
 @author: jaisi8631
 """
 
+
 """
-PRIOR TO RUNNING THIS FILE:
-1. Download dataset via get_iEEG_data.py.
-2. 
-    
-    
+RUNNING THIS FILE FOR DATASET WITH IDENTIFICATION NUMBER 'ID':
+1. Download datasets via get_iEEG_data.py, and name them: 
+   hupID-interictal.pickle and hupID-ictal.pickle
+2. Create csv file with labels, and name it: hupID-labels.csv
+3. 
+
 """
 
+# ------------------
+# CONSTANTS
+# ------------------
+START_TIME_INTERICTAL = 356850680000
+END_TIME_INTERICTAL = 356903099171
+START_TIME_ICTAL = 402704260829
+END_TIME_ICTAL = 402756680000
+FS = 1024
+DOWN_SAMPLE_FACTOR = 10
+STEP_SIZE = 256
+SEQUENCE_LEN = 1024
+
+PATH_INTERICTAL = "../datasets/hup172-interictal.pickle"
+PATH_ICTAL = "../datasets/hup172-ictal.pickle"
+LABELS = "labels/hup172-labels.csv"
+
+
+# ------------------
+# LOAD DATA
+# ------------------
+def get_data(path):
+    with open(path, 'rb') as f: data, fs = pickle.load(f)
+    labels = pd.read_csv("labels/hup138-labels.csv", header = None)
+    labels_list = labels[0].tolist()
+    data = data[data.columns.intersection(labels_list)]
+    return data
+
+def iEEG_data_filter(data, fs, cutoff1, cutoff2, notch):
+	column_names = data.columns
+	data = np.array(data)
+	number_of_channels = data.shape[1]
+    
+    # Cut-off frequency of the filter
+	fc = np.array([cutoff1, cutoff2])
+    # Normalize the frequency
+	w = fc / np.array([(fs / 2), (fs / 2)])  
+    
+	b, a = signal.butter(4, w, 'bandpass')
+	filtered = np.zeros(data.shape)
+	for i in np.arange(0,number_of_channels):
+		filtered[:,i] = signal.filtfilt(b, a, data[:,i])
+	filtered = filtered + (data[0] - filtered[0])
+	f0 = notch
+	q = 30
+	b, a = signal.iirnotch(f0, q, fs)
+	notched = np.zeros(data.shape)
+	for i in np.arange(0, number_of_channels):
+		notched[:, i] = signal.filtfilt(b, a, filtered[:, i])
+	notched_df = pd.DataFrame(notched, columns=column_names)
+	return notched_df
+
+
+# ------------------
+# DATASET OUTLINE
+# ------------------
+def create_timestamps(data):
+    timestamps = []
+    for i in range(0, data.shape[0]):
+        timestamps.append(START_TIME_ICTAL + (i * FS))
+    return timestamps
+
+
+# ------------------
+# PROCESS DATA
+# ------------------
+# DEPRECATED: only for single, continous start and end time
 def create_dataset(data, start_time, end_time):
     dataset = []
     dataset_targets = []
@@ -55,3 +123,36 @@ def create_dataset(data, start_time, end_time):
     dataset_targets = np.array(dataset_targets)
     
     return dataset, dataset_targets
+
+# ------------------
+# MAIN METHOD
+# ------------------
+if __name__=="__main__":
+        
+    # --------------------
+    # DATA WRANGLING
+    # --------------------
+    # interictal
+    data = get_data(PATH_INTERICTAL)
+    timestamps = create_timestamps(data)
+    data_filtered = iEEG_data_filter(data, FS, 0.16, 200, 60)
+    fs_downSample = FS / DOWN_SAMPLE_FACTOR
+    data_filtered_tmp = signal.decimate(data_filtered, DOWN_SAMPLE_FACTOR, axis = 0)
+    data_filtered = pd.DataFrame(data_filtered_tmp, columns = data_filtered.columns); del data_filtered_tmp
+    data_interictal = data_filtered
+    
+    # ictal
+    data = get_data(PATH_ICTAL)
+    timestamps = create_timestamps(data)
+    data_filtered = iEEG_data_filter(data, FS, 0.16, 200, 60)
+    fs_downSample = FS / DOWN_SAMPLE_FACTOR
+    data_filtered_tmp = signal.decimate(data_filtered, DOWN_SAMPLE_FACTOR, axis = 0)
+    data_filtered = pd.DataFrame(data_filtered_tmp, columns = data_filtered.columns); del data_filtered_tmp
+    data_ictal = data_filtered
+    
+    # concatenate
+    data = pd.concat([data_interictal, data_ictal], ignore_index = True)
+    dataset, dataset_targets = create_merge_dataset(data, data_interictal.shape[0],
+                                              START_TIME_INTERICTAL, END_TIME_INTERICTAL,
+                                              START_TIME_ICTAL, END_TIME_ICTAL)
+    
